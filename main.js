1f2e9da51d36eed0eb5e948dea65479b5cbd373c
Peter Garmider <petergarmider@gmail.com>
2020-09-08 19:57:49 -0400

    completed willLikelySurvive function which checks if 60% or greater of the items in the dna array are C or G bases

--- main.js
+++ main.js
@@ -15,11 +15,13 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+//factory function which generates organism objects containing the specimen number, dna bases array, a function to mutate the dna bases array, and a function to compare this object with another organism object
 const pAequorFactory = (organismNum, dnaBaseArray) => {
   return {
     specimenNum: organismNum,
     dna: dnaBaseArray,
 
+    //mutate the DNA array by swapping a random item in the array with another random item from the dnaBases array
     mutate() {
       const randomIndex = Math.floor(this.dna.length * Math.random());
       const filteredDnaList = dnaBases.filter(base => base != this.dna[randomIndex]);
@@ -29,8 +31,8 @@ const pAequorFactory = (organismNum, dnaBaseArray) => {
       return this.dna;
     },
 
+    //compare this.dna of this object to that of another pAequor object and return a % of similarity
     compareDNA(pAequorObject) {
-      //compare this.dna of this object to that of another pAequor object
       let count = 0;
       for (i of this.dna) {
         if (i === pAequorObject.dna[this.dna.indexOf(i)]) {
@@ -43,16 +45,41 @@ const pAequorFactory = (organismNum, dnaBaseArray) => {
       console.log(count);
       console.log(percentDiff);
       console.log(`Specimen ${this.specimenNum} and specimen ${pAequorObject.specimenNum} have ${percentDiff}% DNA in common.`)
+    },
+
+    //Look at this.dna's array and determine if at least 60% of the array is C or G bases
+    willLikelySurvive() {
+      countC = 0;
+      countG = 0;
+      for (i of this.dna) {
+        if (i === 'C') {
+          countC += 1;
+        }
+        else if (i === 'G') {
+          countG += 1;
+        }
+      }
+      console.log(countC);
+      console.log(countG);
+
+      if (countC/15 >= 0.6 || countG/15 >= 0.6) {
+        console.log('yes');
+        return true;
+      }
+      else {
+        console.log('no');
+        return false;
+      }
     }
 
-
-    }
-  };
+  }
+};
 
 const test1 = pAequorFactory(1, mockUpStrand());
-const test2 = pAequorFactory(2, mockUpStrand());
+//const test2 = pAequorFactory(2, mockUpStrand());
+//test1.compareDNA(test2);
 
-test1.compareDNA(test2);
+test1.willLikelySurvive();
 
 //console.log(test1);
 //console.log(test1.mutate());
