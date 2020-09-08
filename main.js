a29bc53f7d0c793af96fd9fb125cf942391ca34f
Peter Garmider <petergarmider@gmail.com>
2020-09-08 19:42:23 -0400

    solved compareDNA function comparing two dna arrays and output the % difference between them

--- main.js
+++ main.js
@@ -19,16 +19,40 @@ const pAequorFactory = (organismNum, dnaBaseArray) => {
   return {
     specimenNum: organismNum,
     dna: dnaBaseArray,
+
     mutate() {
       const randomIndex = Math.floor(this.dna.length * Math.random());
-      console.log(randomIndex);
       const filteredDnaList = dnaBases.filter(base => base != this.dna[randomIndex]);
+
       this.dna.splice(randomIndex, 1, returnRandBase(filteredDnaList));
+
       return this.dna;
-      }
+    },
+
+    compareDNA(pAequorObject) {
+      //compare this.dna of this object to that of another pAequor object
+      let count = 0;
+      for (i of this.dna) {
+        if (i === pAequorObject.dna[this.dna.indexOf(i)]) {
+          console.log(i);
+          count += 1;
+        };
+
+      };
+      const percentDiff = ((count/15)*100).toFixed(2);
+      console.log(count);
+      console.log(percentDiff);
+      console.log(`Specimen ${this.specimenNum} and specimen ${pAequorObject.specimenNum} have ${percentDiff}% DNA in common.`)
+    }
+
+
     }
   };
 
 const test1 = pAequorFactory(1, mockUpStrand());
-console.log(test1);
-console.log(test1.mutate());
+const test2 = pAequorFactory(2, mockUpStrand());
+
+test1.compareDNA(test2);
+
+//console.log(test1);
+//console.log(test1.mutate());
