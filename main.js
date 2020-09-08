90cdc434fccdb2224b2d1dd791f2d0b1d04d7220
Peter Garmider <petergarmider@gmail.com>
2020-09-08 18:34:21 -0400

    solved pAequorFactory logic to replace random dna base in dna array without duplication of same base

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
 // Returns a random DNA base
-const returnRandBase = () => {
-  const dnaBases = ['A', 'T', 'C', 'G'];
-  return dnaBases[Math.floor(Math.random() * 4)];
+
+const dnaBases = ['A', 'T', 'C', 'G'];
+
+const returnRandBase = (baseList) => {
+  return baseList[Math.floor(Math.random() * baseList.length)];
 };
 
 // Returns a random single stand of DNA containing 15 bases
 const mockUpStrand = () => {
   const newStrand = [];
   for (let i = 0; i < 15; i++) {
-    newStrand.push(returnRandBase());
+    newStrand.push(returnRandBase(dnaBases));
   }
   return newStrand;
 };
 
+const pAequorFactory = (organismNum, dnaBaseArray) => {
+  return {
+    specimenNum: organismNum,
+    dna: dnaBaseArray,
+    mutate() {
+      const randomIndex = Math.floor(this.dna.length * Math.random());
+      console.log(randomIndex);
+      const filteredDnaList = dnaBases.filter(base => base != this.dna[randomIndex]);
+      this.dna.splice(randomIndex, 1, returnRandBase(filteredDnaList));
+      return this.dna;
+      }
+    }
+  };
 
-
-
-
-
-
-
+const test1 = pAequorFactory(1, mockUpStrand());
+console.log(test1);
+console.log(test1.mutate());
